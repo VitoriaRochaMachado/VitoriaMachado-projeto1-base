78c45a5d93db0eac5b0a9f27556a913c4cc10cca
Vitoria Rocha Machado <vitoriarm@al.insper.edu.br>
2026-08-14 13:22:42 -0300

    README com link e handout finalizado

--- servidor.py
+++ servidor.py
@@ -1,49 +1,28 @@
-from flask import Flask, render_template_string, url_for
-from utils import load_data
+from flask import Flask, render_template_string, request, redirect
+import views
 
 
 app = Flask(__name__)
-
-NOTE_TEMPLATE = '''  <li>
-    <h3>{title}</h3>
-    <p>{details}</p>
-  </li>
-'''
-
-RESPONSE_TEMPLATE = '''<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Get-it</title>
-</head>
-<body>
-
-<img src="{{{{ url_for('static', filename='img/logo-getit.png') }}}}">
-<p>Como o Post-it, mas com outro verbo</p>
-
-<ul>
-{notes}
-</ul>
-
-</body>
-</html>
-'''
 
 # Configurando a pasta de arquivos estáticos
 app.static_folder = 'static'
 
 @app.route('/')
 def index():
-    notes_li = [
-        NOTE_TEMPLATE.format(title=dados['titulo'], details=dados['detalhes'])
-        for dados in load_data('notes.json')
-    ]
-    notes = '\n'.join(notes_li)
 
-    response = RESPONSE_TEMPLATE.format(notes=notes)
+    return render_template_string(views.index())
 
-    return render_template_string(response)
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    titulo = request.form.get('titulo')  # Obtém o valor do campo 'titulo'
+    detalhes = request.form.get('detalhes')  # Obtém o valor do campo 'detalhes'
 
+    views.submit(titulo, detalhes)
+    return redirect('/')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return views.not_found()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -5,3 +5,15 @@ def load_data(notes):
     with open(caminho, "r", encoding="utf-8") as arquivo:
         dados = json.load(arquivo)
     return dados
+
+def load_template(index):
+    with open( "static/templates/" + index, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def save_note(nova_anotacao):
+    dados = load_data("notes.json")
+    dados.append(nova_anotacao)
+
+    with open("static/data/notes.json", "w", encoding="utf-8") as arquivo:
+        json.dump(dados, arquivo, ensure_ascii=False, indent=4)
